0222fb0d1d47ddce0567aa2d1ce5a7e42e0fc332
mariejosea <marie1danez@gmail.com>
2020-12-13 01:44:19 -0400

    generator finished

--- src/main.js
+++ src/main.js
@@ -6,5 +6,51 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  newCard();
+  document.querySelector(".button").addEventListener("click", newCard);
+};
+
+let topNumber = document.querySelector(".top-number");
+let middleSuit = document.querySelector(".middle-suit");
+let buttomNumber = document.querySelector(".bottom-number");
+let card = document.querySelector(".card");
+
+function newCard() {
+  let colorSuit = generateRandomSuit();
+  if (colorSuit === "♥" || colorSuit === "♦") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
+  let number = generateRandomNumber();
+
+  topNumber.innerHTML = number;
+  middleSuit.innerHTML = colorSuit;
+  buttomNumber.innerHTML = number;
+}
+
+let generateRandomSuit = () => {
+  let suit = ["♥", "♦", "♣", "♠"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+};
+
+let generateRandomNumber = () => {
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNumber = Math.floor(Math.random() * values.length);
+  return values[randomNumber];
 };
